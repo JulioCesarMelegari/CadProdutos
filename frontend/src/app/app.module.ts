dff4430c72afaf265e533567e02e507ee9b17996
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-09-28 14:29:23 -0300

    diretiva formatação preço

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,6 +28,7 @@ import localePt from '@angular/common/locales/pt'
 import { registerLocaleData } from '@angular/common';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ConverteNumerosDirective } from './components/directives/converte-numeros.directive';
 
 registerLocaleData(localePt);
 
@@ -42,7 +43,8 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductUpdateComponent,
-    ProductDeleteComponent
+    ProductDeleteComponent,
+    ConverteNumerosDirective
   ],
   imports: [
     BrowserModule,
